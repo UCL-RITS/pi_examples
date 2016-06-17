3116367547d302d39811a53b383fd5a10fcd3097
Ian Kirker <ian.kirker@gmail.com>
2016-06-17 15:29:07 +0100

    Adds lua version

---
+++ lua_pi_dir/pi.lua
@@ -0,0 +1,27 @@
+
+local i=0
+local num_steps = 100000000
+
+local sum = 0.0
+local step = 1.0 / num_steps
+
+print("Calculating pi using:\n  "..num_steps.." slices\n  1 process")
+
+-- This timer only has a clock resolution of 10 milliseconds apparently but it's
+--  the best we can do with ordinary lua.
+local start_time = os.clock()
+
+for i = 0, num_steps do
+    x = (i - 0.5) * step
+    sum = sum + (4.0 / (1.0 + x*x))
+end
+
+local pi = sum * step
+
+local end_time = os.clock()
+
+local duration = end_time - start_time
+
+print("Obtained value for PI: "..pi.."\n"..
+      "Time taken: "..duration.." seconds")
+
