8210f3019b59a4e3b6681b1edcc2a71a6b2e93ae
Owain Kenway <o.kenway@ucl.ac.uk>
2016-06-27 11:01:01 +0100

    Remove extra .

--- pascal_pi_dir/pi.pas
+++ pascal_pi_dir/pi.pas
@@ -15,7 +15,7 @@ Begin
   if ParamCount > 0 then
     numsteps := StrToInt64(ParamStr(1));
 
-  Writeln('Calculating PI using: ', numsteps, ' slices.');
+  Writeln('Calculating PI using: ', numsteps, ' slices');
 
 { Initialise time counter and sum. Set step size. }
   start := TimeStampToMSecs(DateTimeToTimeStamp(Now)); 
