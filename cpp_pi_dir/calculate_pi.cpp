2fcff2615ad19d747354a7952c854a8c840cf6fe
Ian Kirker <ian.kirker@gmail.com>
2016-09-28 21:55:50 +0100

    Adds naive C++ version
    I'm not very experienced in C++. Might ask for someone more experienced
    to take a look at this one.

---
+++ cpp_pi_dir/calculate_pi.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+#include <limits>
+
+#include "timer.hpp"
+
+using namespace std;
+
+int main( int argc, char **argv ) {
+   long i, num_steps = 1000000000;
+
+   cout.precision(numeric_limits<double>::digits10+2);
+   
+   if (argc > 1) {
+      num_steps = atol(argv[1]);
+   }
+
+   double step, x, sum, pi;
+   Timer timer;
+   
+   cout << "Calculating PI using:" << endl <<
+           "  " << num_steps << " slices" << endl <<
+           "  1 process" << endl;
+   
+   timer.start();
+   
+   sum = 0.0;
+   step = 1.0 / num_steps;
+
+   for (i=0;i<num_steps;i++) {
+      x    = (i + 0.5) * step;
+      sum += 4.0 / (1.0 + x*x);
+   }
+
+   pi = sum * step;
+
+   timer.stop();
+
+   cout << "Obtained value for PI: " << pi << endl <<
+           "Time taken: " << timer.duration() << " seconds" << endl;
+
+   return 0;
+}
+
