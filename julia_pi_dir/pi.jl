747d2d8d3ec3635692993ed378a9b7939fe839b7
Owain Kenway <o.kenway@ucl.ac.uk>
2016-10-03 17:25:54 +0100

    Parallel implementation.

--- julia_pi_dir/pi.jl
+++ julia_pi_dir/pi.jl
@@ -6,17 +6,24 @@ if length(ARGS) > 0
   numsteps = parse(ARGS[1])
 end
 
+# Get number of processes.
+# Note: In 0.4.5 on Ubuntu, the --help entry for
+# -p is wrong.  
+# -p <n> adds <n> additional procs for n >=1
+# By default runs 1 core, not all available
+np = nprocs()
+
 println("Calculating PI using:")
 println("  ", numsteps, " slices")
-
+println("  ", np, " processe(s)")
 start = time()
 
 sum = 0.0
 slice = 1.0/numsteps
 
-for i = 1:numsteps
+sum = @parallel (+) for i = 1:numsteps
   x = (i - 0.5) * slice
-  sum = sum + (4.0/(1.0 + x^2))
+  (4.0/(1.0 + x^2))
 end
 
 mypi = sum * slice
