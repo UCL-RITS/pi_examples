6efb26f012f890950eff23bb93fa8b1f8b45b07f
Owain Kenway <o.kenway@ucl.ac.uk>
2016-10-26 16:30:43 +0100

    Fix the off by 0.5 problem.
    This made the function calcp too complex for the poor swift compiler so I've
    split it into to - gen(n) which generates an array and calcp(n) which does the
    integration.
    
    We have to split this into two functions otherwise the Swift 3.x compiler
    complains:
    
    error: expression was too complex to be solved in reasonable time; consider breaking up the expression into distinct sub-expressions
    
    Looking at stackoverflow this is a... feature of the compiler:
       http://stackoverflow.com/questions/29707622/bizarre-swift-compiler-error-expression-too-complex-on-a-string-concatenation

--- swift_func_pi_dir/pi.swift
+++ swift_func_pi_dir/pi.swift
@@ -4,7 +4,15 @@ import Foundation
 
 let y:(Double) -> Double = {x in 1.0/(1.0+(x*x))}
 
-func calcp(n: Int) -> Double { return ((1...n).map{Double($0)/Double(n)}.map(y).reduce(0,+))*(4.0/Double(n)) }
+// We have to split this into two functions otherwise the Swift 3.x compiler
+// complains:
+//
+// error: expression was too complex to be solved in reasonable time; consider breaking up the expression into distinct sub-expressions
+//
+// Looking at stackoverflow this is a... feature of the compiler:
+//   http://stackoverflow.com/questions/29707622/bizarre-swift-compiler-error-expression-too-complex-on-a-string-concatenation
+func gen(n: Int) -> [Double] { return (1...n).map{Double($0)-0.5} }
+func calcp(n: Int) -> Double { return (gen(n:n).map{Double($0)/Double(n)}.map(y).reduce(0,+))*(4.0/Double(n)) }
 
 var num_steps: Int = 10000000
 
