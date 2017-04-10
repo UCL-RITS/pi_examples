ab69592162e8cc4da5c4ee6171932cd6691f4260
Owain Kenway <o.kenway@ucl.ac.uk>
2017-04-10 11:33:41 +0100

    Added prefix "pi-" to variable names.
    This is "good e-LISP style" and prevents this sort of warning from the byte
    code compiler:
    
    In toplevel form:
    elisp-pi.el:3:1:Warning: global/dynamic var `n' lacks a prefix
    elisp-pi.el:15:1:Warning: global/dynamic var `start' lacks a prefix
    elisp-pi.el:17:1:Warning: global/dynamic var `sum' lacks a prefix
    elisp-pi.el:18:1:Warning: global/dynamic var `st' lacks a prefix
    elisp-pi.el:19:1:Warning: global/dynamic var `x' lacks a prefix
    elisp-pi.el:27:1:Warning: global/dynamic var `p' lacks a prefix
    elisp-pi.el:29:1:Warning: global/dynamic var `stop' lacks a prefix
    elisp-pi.el:31:1:Warning: global/dynamic var `runt' lacks a prefix

--- elisp_pi_dir/elisp-pi.el
+++ elisp_pi_dir/elisp-pi.el
@@ -1,37 +1,37 @@
 ; PI Emacs LISP example.
 
-(defvar n 5000000)
+(defvar pi-n 5000000)
 
 (if (> (length command-line-args-left) 0)
-  (setq n (string-to-number (pop command-line-args-left)))
+  (setq pi-n (string-to-number (pop command-line-args-left)))
 )
 
 (princ "Calculating PI using:\n")
-(princ (format "  %d slices\n" n))
+(princ (format "  %d slices\n" pi-n))
 (princ "  1 process\n")
 
 ; current-time returns a 4-ple but we can just use the time-subtract
 ;  and float-time functions below and mostly ignore that
-(defvar start (current-time))
+(defvar pi-start (current-time))
 
-(defvar sum 0.0)
-(defvar st (/ 1.0 n))
-(defvar x 0.0)
+(defvar pi-sum 0.0)
+(defvar pi-st (/ 1.0 pi-n))
+(defvar pi-x 0.0)
 
 ; dotimes can only start at 0 as far as I can tell.
-(dotimes (i (- n 1))
-    (setq x (* (+ i 0.5) st))
-    (setq sum (+ sum (/ 4.0 (+ 1 (* x x)))))
+(dotimes (i (- pi-n 1))
+    (setq pi-x (* (+ i 0.5) pi-st))
+    (setq pi-sum (+ pi-sum (/ 4.0 (+ 1 (* pi-x pi-x)))))
 )
 
-(defvar p (* sum st))
+(defvar pi-p (* pi-sum pi-st))
 
-(defvar stop (current-time))
+(defvar pi-stop (current-time))
 
-(defvar runt (float-time (time-subtract stop start)))
+(defvar pi-runt (float-time (time-subtract pi-stop pi-start)))
 
-(princ (format "Obtained value of PI: %.16g\n" p))
+(princ (format "Obtained value of PI: %.16g\n" pi-p))
 
 ; We have to print the time with ~f to force a float output.
-(princ (format "Time taken: %g seconds\n" runt))
+(princ (format "Time taken: %g seconds\n" pi-runt))
 
