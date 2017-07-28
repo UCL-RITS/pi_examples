6d7569f829e0e6fe96d45750cd1231060de2b2cb
Owain Kenway <o.kenway@ucl.ac.uk>
2017-07-28 11:44:00 +0100

    Autodetect Common LISP implementation.

--- clisp_pi_dir/run.sh
+++ clisp_pi_dir/run.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-# Run
-PI_ARG=$1 make run
+# Detect which if any Common LISP implementation we have:
+which sbcl > /dev/null 2>&1
+sbclsuccess=$?
 
+which clisp > /dev/null 2>&1
+clispsuccess=$?
+
+if [ "$sbclsuccess" == "0" ]; then
+  sbcl --script pi.cl $1
+elif [ "$clispsuccess" == "0" ]; then 
+  PI_ARG=$1 make run
+else
+  echo "No Common LISP implementation found."
+  exit 1
+fi
