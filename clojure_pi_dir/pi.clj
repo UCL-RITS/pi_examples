2c1b8e4cdfdbaac97748886685c27172babe410a
Owain Kenway <o.kenway@ucl.ac.uk>
2017-08-09 13:01:01 +0100

    Some comments.

--- clojure_pi_dir/pi.clj
+++ clojure_pi_dir/pi.clj
@@ -1,12 +1,15 @@
+; We have to define name-space etc. because we want to compile to to Java byte-code.
 (ns pi
   (:gen-class
    :methods [[calcpi [] Integer]]))
 
+; The method that does the bulk of our work as usual.
 (defn calcpi [n]
   (println "Calculating PI using:")
   (println " " n "slices")
   (println "  1 processes")
 
+; When referring to Java stuff the syntax is class/method.
   (def start (System/currentTimeMillis))
   (def psum 0.0) 
   (def step (/ 1 n))
@@ -15,7 +18,6 @@
     (def x (* (+ i 0.5) step))
     (def psum (+ psum (/ 4 (+ 1 (* x x)))))
     (recur (inc i))
-
   ))
 
   (def mypi (* psum step))
--- clojure_pi_dir/run.clj
+++ clojure_pi_dir/run.clj
@@ -1,4 +1,8 @@
+; This is a wrapper Clojure script that checks command line args and calls the
+; methods we have defined in pi.clj.
 
+; We could actually build in this script because of the way LISPs like Clojure
+; work but that would mean building every time, so leave it to Make.
 
 (def n 10000000)
 (if (> (count *command-line-args*) 0)
