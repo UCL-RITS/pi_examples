49366e8f87fe285ccb6bdba2b410061b78cf5a52
Owain Kenway <o.kenway@ucl.ac.uk>
2017-10-31 16:47:48 +0000

    switch to using a generator
    lists are big.  big is bad.  generator is slower than list until list fills memory.

--- python_functional_pi_dir/pi.py
+++ python_functional_pi_dir/pi.py
@@ -1,21 +1,33 @@
 def y(x):
     return 1.0/(1.0 + (x**2))
 
-def calcpi(n):
-    return (4.0/n) * sum(map(y, [(x+0.5)/n for x in range(n)]))
+def calcpi(n, r):
+    return (4.0/n) * sum(map(y, r))
 
+# Use a generator to save memory (lists are huge!).
+def gen_x(n):
+    i = 0.5
+    while (i < n):
+        v = (yield i/n)
+        if v is not None:
+            i = v
+        else:
+            i = i + 1
+        
+# In lieu of a main.
 if __name__=='__main__':
     import sys
     import time
 
-    num_steps = 10000000
+    num_steps = 5000000
 
     if len(sys.argv) > 1:
         num_steps = int(sys.argv[1])
 
     print('Calculating PI with:\n  ' + str(num_steps) + ' slices')
     start =  time.time()
-    p = calcpi(num_steps)
+    gen = gen_x(num_steps)
+    p = calcpi(num_steps, gen)
     t = time.time() - start
     print('Obtained value of PI: ' + str(p))
     print('Time taken:           ' + str(t) + ' seconds')
