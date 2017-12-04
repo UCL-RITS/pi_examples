3286587e16d6a9d2a61d028d26fe44bd5ab671bc
Owain Kenway <o.kenway@ucl.ac.uk>
2017-12-04 11:32:45 +0000

    Remove :methods
    (apparently unnecessary?)

--- clojure_pi_dir/pi.clj
+++ clojure_pi_dir/pi.clj
@@ -1,7 +1,6 @@
 ; We have to define name-space etc. because we want to compile to to Java byte-code.
 (ns pi
-  (:gen-class
-   :methods [[calcpi [] Integer]]))
+  (:gen-class))
 
 ; The function that does the bulk of our work as usual.
 (defn calcpi [n] ((fn [x] (/ (apply + (map (fn [x] (/ 4 (+ 1 (* x x)))) (range (/ 0.5 x) 1 (/ 1 x)))) x)) n))
