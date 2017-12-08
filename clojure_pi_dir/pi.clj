ee5d057d9fce5a643d9c4faeecca893c0a101107
Owain Kenway <o.kenway@ucl.ac.uk>
2017-12-08 11:35:33 +0000

    Typo.

--- clojure_pi_dir/pi.clj
+++ clojure_pi_dir/pi.clj
@@ -1,7 +1,7 @@
 ; We have to define name-space etc. because we want to compile to to Java byte-code.
 (ns pi
   (:gen-class
-  :methods [#^{t:static true} [calcpi [int] double]]))
+  :methods [#^{:static true} [calcpi [int] double]]))
 
 ; The function that does the bulk of our work as usual.
 (defn -calcpi [n] ((fn [x] (/ (apply + (map (fn [x] (/ 4 (+ 1 (* x x)))) (range (/ 0.5 x) 1 (/ 1 x)))) x)) n))
