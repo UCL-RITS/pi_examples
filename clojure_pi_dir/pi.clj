c1ab1ede219b336887d49ae49d15345e63a9abaa
Owain Kenway <o.kenway@ucl.ac.uk>
2017-12-08 11:31:35 +0000

    Fix method declarations for Clojure 1.8
    So we do need :methods for Clojure 1.8. Worse, we need to make them static and
    prepend them with - which breaks on Clojure 1.6.
    So we need a slight cludge where there's a method without the - which calls the one with.
    
    Make clean was also slightly broken if pi.jar wasn't up to date - fixed.

--- clojure_pi_dir/pi.clj
+++ clojure_pi_dir/pi.clj
@@ -1,6 +1,9 @@
 ; We have to define name-space etc. because we want to compile to to Java byte-code.
 (ns pi
-  (:gen-class))
+  (:gen-class
+  :methods [#^{t:static true} [calcpi [int] double]]))
 
 ; The function that does the bulk of our work as usual.
-(defn calcpi [n] ((fn [x] (/ (apply + (map (fn [x] (/ 4 (+ 1 (* x x)))) (range (/ 0.5 x) 1 (/ 1 x)))) x)) n))
+(defn -calcpi [n] ((fn [x] (/ (apply + (map (fn [x] (/ 4 (+ 1 (* x x)))) (range (/ 0.5 x) 1 (/ 1 x)))) x)) n))
+
+(defn calcpi [n] (-calcpi n))
