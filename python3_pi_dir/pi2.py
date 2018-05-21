de3b186a7292030ff73131f52bee1ac740652179
Owain Kenway <o.kenway@ucl.ac.uk>
2018-05-21 13:55:53 +0100

    Python 3 versions.

---
+++ python3_pi_dir/pi2.py
@@ -0,0 +1,26 @@
+# Version of the Python Pi example compatible 3 optimised.
+
+# Dr Owain Kenway
+
+import time
+import sys
+
+
+num_steps = 10000000
+
+if len(sys.argv) > 1:
+	num_steps = int(sys.argv[1])
+
+print("Calculating PI with: " + str(num_steps) + " slices")
+
+total_sum = 0
+step = 1.0 / num_steps
+
+start = time.time()
+
+p = step * sum(4.0/(1.0 + ((i + 0.5) * (i + 0.5) * step * step)) for i in range(num_steps))
+
+stop = time.time()
+
+print("Obtained value of PI: " + str(p))
+print("Time taken: " + str(stop - start) + " seconds")
