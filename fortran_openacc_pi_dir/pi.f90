fc88e09cb1bf1954c93068104d0d69435ae5fe0f
Owain Kenway <o.kenway@ucl.ac.uk>
2018-05-31 13:08:44 +0100

    OpenACC version.

---
+++ fortran_openacc_pi_dir/pi.f90
@@ -0,0 +1,55 @@
+program pi
+
+  use ISO_FORTRAN_ENV
+
+  implicit none
+  
+  double precision              :: step, x, s, mypi, start, stop, omp_get_wtime
+  integer(kind=int64)           :: num_steps, i
+  character(len=:), allocatable :: a
+  integer, external             :: omp_get_max_threads
+  integer                       :: argl
+
+  num_steps = 10000000000
+
+! Get command line args (Fortran 2003 standard)
+  if (command_argument_count() > 0) then
+     call get_command_argument(1, length=argl)
+     allocate(character(argl) :: a)
+     call get_command_argument(1, a)
+     read(a,*) num_steps
+  end if
+
+! Output start message
+
+  write(*,'(A)') "Calculating PI using:"
+  write(*,'(A,1I16,A)') "                  ",num_steps, " slices"
+  write(*,'(A,1I16,A)') "                  ",omp_get_max_threads()," OpenMP threads"
+
+! Initialise time counter and sum: set step size
+
+  start = omp_get_wtime()
+  s = 0d0
+  step = 1.0d0 / num_steps
+
+! Specify that loops within this region should have kernels built for GPU
+
+!$ACC KERNELS
+  do i = 1, num_steps
+    x = (i - 0.5d0) * step
+    s = s + 4.0d0 / (1.0d0 + x*x)
+  end do
+!$ACC END KERNELS
+
+! Evaluate PI from the final sum value, and stop the clock
+
+  mypi = s * step
+  stop = omp_get_wtime()
+
+! output value of PI and time taken
+
+  write(*,'(A,1F12.10,A)') "Obtained value of PI: ", mypi
+  write(*,'(A,1F12.5,A)') "Time taken:           ",(stop-start), " seconds"
+
+end program pi
+
---
+++ fortran_openacc_pi_dir/sub.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#$ -l gpu=1
+#$ -l h_rt=2:0:0
+#$ -cwd
+
+module purge
+module load rcps-core
+module load compilers/gnu
+#module load cuda/8.0.61-patch2/gnu-4.9.2
+module load cuda/7.5.18/gnu-4.9.2
+module remove compilers/gnu
+module load compilers/pgi/2017.3
+
+hostname
+
+nvidia-smi
+pgaccelinfo 
+# ls -alh /dev/*
+
+make clean
+make pgi
+
+./pi
