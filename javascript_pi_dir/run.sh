31cc5a6a6be7e9ea2773b600518b30689f6d133a
Owain Kenway <o.kenway@ucl.ac.uk>
2018-07-30 13:40:12 +0100

    On some systems nodejs is simply node.

--- javascript_pi_dir/run.sh
+++ javascript_pi_dir/run.sh
@@ -8,11 +8,17 @@ d8success=$?
 which nodejs > /dev/null 2>&1
 nodesuccess=$?
 
+# See if node.js is in our path:
+which node > /dev/null 2>&1
+nodesuccess2=$?
+
 
 if [ "$d8success" == "0" ]; then
   d8 rund8.js -- $1
 elif [ "$nodesuccess" == "0" ]; then
   nodejs runnode.js $1
+elif [ "$nodesuccess2" == "0" ]; then
+  node runnode.js $1
 else
   echo "No JavaScript Interpreter found."
   exit 1
