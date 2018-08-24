a0d706cd19f7613066ac451411182a63c4793378
Owain Kenway <o.kenway@ucl.ac.uk>
2018-08-24 12:45:37 +0100

    Set maxNumCompThreads on Octave launch.

--- matlab_pi_dir/run.sh
+++ matlab_pi_dir/run.sh
@@ -18,7 +18,7 @@ if [ "$mlsuccess" == "0" ] ; then
 #  matlab -nodisplay -nodesktop -nosplash -nojvm -r "${n}run('calcpi.m');quit;"
   matlab -singleCompThread -nodisplay -nodesktop -nosplash -nojvm -r "${n}run('calcpi.m');quit;"
 elif [ "$ocsuccess" == "0" ] ; then
-  octave --no-gui --silent --eval "${n}run('calcpi.m');"
+  octave --no-gui --silent --eval "maxNumCompThreads=1;${n}run('calcpi.m');"
 else
   echo "No Matlab interpreter found."
 fi
