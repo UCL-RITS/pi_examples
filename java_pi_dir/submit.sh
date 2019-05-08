20f226f740fd68ab76fc68834f616163b2908565
Owain Kenway <o.kenway@ucl.ac.uk>
2019-05-08 11:33:31 +0100

    Script to benchmark JDKs.

---
+++ java_pi_dir/submit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#$ -N JDKbench
+#$ -t 00:20:00
+#$ -l mem=4G
+
+#$ -cwd
+
+echo > timing.csv
+
+for a in `module avail java -t 2>&1 | grep java`
+do
+  make clean
+  module load $a
+  make
+  for b in `seq -w 10`
+  do
+    t=`java pi | grep Time | awk '{print $3}'`
+    echo "${a}|${b}|${t}" >> timing.csv
+  done
+  module remove $a
+done
