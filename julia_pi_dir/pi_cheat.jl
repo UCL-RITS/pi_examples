4dc9d84edf1a7037768b80c8fed69718feb505d5
Owain Kenway <o.kenway@ucl.ac.uk>
2020-02-12 16:34:03 +0000

    Julia version that calls picalc outside timing.
    For reasons Julia jits stuff as it interprets it rather than jiting ahead of time so we can trick it into doing that by calling a 1 iteration version of picalc.

---
+++ julia_pi_dir/pi_cheat.jl
@@ -0,0 +1,47 @@
+
+function work(numsteps, np)
+  sum = 0.0
+  slice = 1.0/numsteps
+
+  sum = @distributed (+) for i = 1:numsteps
+    x = (i - 0.5) * slice
+    (4.0/(1.0 + x^2))
+  end
+
+  mypi = sum * slice
+
+  return mypi
+end 
+
+function picalc(numsteps)
+
+  # Get number of processes.
+  np = nworkers()
+
+  println("Calculating PI using:")
+  println("  ", numsteps, " slices")
+  println("  ", np, " worker(s)")
+  temp = work(1,1)
+  temp = work(numsteps,np)
+  start = time()
+
+  mypi = work(numsteps,np)
+
+  stop = time()
+
+  elapsed = (stop - start)
+
+  println("Obtained value of PI: ", mypi)
+  println("Time taken: ", elapsed, " seconds")
+
+end
+
+numsteps=1000000000
+
+if length(ARGS) > 0
+  numsteps = parse(Int, ARGS[1])
+end
+
+
+picalc(numsteps)
+
