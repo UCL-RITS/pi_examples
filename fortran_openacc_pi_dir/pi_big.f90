4422f1ac6cd0f4404a8097fd169e4c8baf7f9ac3
Owain Kenway <o.kenway@ucl.ac.uk>
2020-10-15 16:03:00 +0100

    Fix pi_big so it actually works

--- fortran_openacc_pi_dir/pi_big.f90
+++ fortran_openacc_pi_dir/pi_big.f90
@@ -10,12 +10,12 @@ program pi_big
   implicit none
   
   double precision              :: step, x, s, mypi, start, stop, omp_get_wtime
-  integer(kind=int64)           :: num_steps
-  character(len=:), allocatable :: a
-  integer                       :: argl, i, j, jmax
+  integer(kind=int64)           :: num_steps, i, j, imax, jmax
+  character(len=:), allocatable :: a, b
+  integer                       :: argl, bargle
   integer(kind=acc_device_kind) :: devices
 
-  num_steps = 100000000
+  imax = 100000000
 
   jmax = 1
 
@@ -24,25 +24,25 @@ program pi_big
      call get_command_argument(1, length=argl)
      allocate(character(argl) :: a)
      call get_command_argument(1, a)
-     read(a,*) num_steps
-     if num_steps > 2147483647 then
+     read(a,*) imax
+     if (imax > 2147483647) then
        write(*,*) "base num_steps > max_int -> this can cause undefined behavior on some OpenACC "
        write(*,*) "targets e.g. multicore"
      end if
   end if
-
+  
   if (command_argument_count() > 1) then
-     call get_command_argument(1, length=argl)
-     allocate(character(argl) :: a)
-     call get_command_argument(1, a)
-     read(a,*) jmax
-     num_steps = num_steps * jmax
-     if jmax > 2147483647 then
+     call get_command_argument(2, length=bargl)
+     allocate(character(bargl) :: b)
+     call get_command_argument(2, b)
+     read(b,*) jmax
+     if (jmax > 2147483647) then
        write(*,*) "multiplicative num_steps > max_int -> this can cause undefined behavior on some"
        write(*,*) "OpenACC targets e.g. multicore"
      end if
   end if
   
+  num_steps = imax * jmax
 
 ! Get 
 
@@ -66,10 +66,10 @@ program pi_big
 ! Specify that loops within this region should have kernels built for GPU
 
 !$ACC PARALLEL
-!$ACC LOOP PRIVATE(x,i,j) REDUCTION(+:s)
-  do i = 1, num_steps/jmax
+!$ACC LOOP PRIVATE(x,i,j,s) REDUCTION(+:s)
+  do i = 1, imax
     do j = 1, jmax
-      x = (((jmax *(i-1))+j) - 0.5d0) * step
+      x = (((jmax * ( i - 1)) + j) - 0.5d0) * step
       s = s + 4.0d0 / (1.0d0 + x*x)
     end do
   end do
