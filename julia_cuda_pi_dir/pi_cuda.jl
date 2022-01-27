41da5c1d72d11ce826e067850fb483cda6b3ba63
Owain Kenway <o.kenway@ucl.ac.uk>
2022-01-27 14:56:33 +0000

    CUDA Julia implementation.

---
+++ julia_cuda_pi_dir/pi_cuda.jl
@@ -0,0 +1,67 @@
+#!/usr/bin/env julia
+
+# Make sure we have all the CUDA gubbins set up.
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+using CUDA
+
+# Actual CUDA kernel.
+function pichunk!(threads, n, result)
+  rank=threadIdx().x
+  lower=floor((rank-1) * n/threads)
+  upper=floor((rank) * n/threads)
+  step = 1.0/n
+  for i=lower:upper
+    x = (i + 0.5) * step
+    result[rank] += 4.0/(1.0 + x*x)
+  end
+  return
+end
+
+# Sort out CUDA thread setup.
+function _picalc(nthr, slices)
+  result = CUDA.fill(0.0, nthr)
+  @cuda threads=nthr pichunk!(nthr, slices, result)
+  synchronize()
+  s = sum(result)/slices
+  return(s)
+end
+
+# Run the benchmark.
+function picalc(numsteps, nthr)
+
+  println("Calculating PI using:")
+  println("  ", numsteps, " slices")
+  println("  ", nthr, " CUDA threads(s)")
+  start = time()
+
+  mypi = _picalc(nthr, numsteps)
+
+  elapsed = time() - start
+
+  println("Obtained value of PI: ", mypi)
+  println("Time taken: ", elapsed, " seconds")
+
+end
+
+# Parse arguments.
+numsteps = if length(ARGS) > 0
+  parse(Int, ARGS[1])
+else
+  1_000_000_000
+end
+
+threads = if length(ARGS) > 1
+  parse(Int, ARGS[2])
+else
+  64
+end
+
+# Warm things up
+print("  Warming up...")
+dummy = _picalc(1,10)
+println("done.\n")
+
+# Run the full example
+picalc(numsteps,threads)
