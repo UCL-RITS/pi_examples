83b92cfbadebf189b3206bc0932c8aaeca2df80f
Owain Kenway <o.kenway@ucl.ac.uk>
2022-04-08 15:22:16 +0100

    Matlab version which uses arrays rather than loops

---
+++ matlab_pi_dir/run_array.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Work out whether to run matlab (preferred) or octave.
+which matlab > /dev/null 2>&1
+mlsuccess=$?
+which octave > /dev/null 2>&1
+ocsuccess=$?
+
+# Default to not setting n
+n=""
+
+# If we have an argument, set n=$1
+if [ $# == 1 ]; then
+  n="n=${1};"
+fi
+
+if [ "$mlsuccess" == "0" ] ; then
+  matlab -singleCompThread -nodisplay -nodesktop -nosplash -nojvm -r "${n}run('calcpi_array.m');quit;"
+elif [ "$ocsuccess" == "0" ] ; then
+  octave --no-gui --silent --eval "maxNumCompThreads=1;${n}run('calcpi_array.m');"
+else
+  echo "No Matlab interpreter found."
+fi
