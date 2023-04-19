fd799f45e72ea372ba4521e2e988881f8a162f91
Owain Kenway <o.kenway@ucl.ac.uk>
2023-04-19 13:40:15 +0100

    More sane summation syntax
    Also comments.

--- c#_pi_dir/pi.cs
+++ c#_pi_dir/pi.cs
@@ -3,14 +3,17 @@ using System.Threading.Tasks;
 
 class Program {
 
+   // Global to turn debug output on and off.
    static bool DEBUG;
 
+   // Helper that prints debug messages if DEBUG.
    static void DebugMessage(String message) {
       if (DEBUG) {
          Console.Error.WriteLine(">>> DEBUG: " + message);
       }
    }
 
+   // Helper that prints warning messages.
    static void WarningMessage(String message) {
       Console.Error.WriteLine(">>> WARNING: " + message);
    }
@@ -19,16 +22,17 @@ class Program {
 
       long numsteps;
       int numthreads;
-      double step, psum, mypi, time;
+      double step, mypi, time;
       DateTime start, stop;
       TimeSpan difference;
       ParallelOptions options;
-      object z = new object();
 
+      // Defaults
       numsteps = 100000000;
       numthreads = 1;
       DEBUG = false;
 
+      // Based value of DEBUG on environment variable PI_DEBUG.
       string? PI_DEBUG = Environment.GetEnvironmentVariable("PI_DEBUG");
       if (PI_DEBUG != null) {
          if (PI_DEBUG.Trim().ToLower().Equals("true") || PI_DEBUG.Trim().Equals("1")) {
@@ -40,10 +44,16 @@ class Program {
          }
       }
 
+      // Base number of steps on argument 0.
       if (argv.Length > 0) {
-         numsteps = Convert.ToInt64(argv[0]);
+         try {
+            numsteps = Convert.ToInt64(argv[0]);
+         } catch {
+            WarningMessage("Ignoring invalid value of first argument: " + argv[0]);
+         }
       }
 
+      // If OMP_NUM_THREADS is set use as number of threads.
       string? OMP_NUM_THREADS = Environment.GetEnvironmentVariable("OMP_NUM_THREADS");
       if (OMP_NUM_THREADS != null) {
          DebugMessage("OMP_NUM_THREADS detected: " + OMP_NUM_THREADS);
@@ -53,13 +63,18 @@ class Program {
          } catch {
             WarningMessage("Ignoring invalid value of OMP_NUM_THREADS: " + OMP_NUM_THREADS);
          }
-         
       }
 
+      // If there is a second argument, set number of threads to that, overriding OMP_NUM_THREADS if set.
       if (argv.Length > 1) {
-         numthreads = Convert.ToInt32(argv[1]);
+         try {
+            numthreads = Convert.ToInt32(argv[1]);
+         } catch {
+            WarningMessage("Ignoring invalid value of second argument: " + argv[1]);
+         }
       }
 
+      // Create a parallel option set that sets the maximum number of current threads.
       options = new ParallelOptions{MaxDegreeOfParallelism = numthreads};
 
       Console.WriteLine("Calculating PI using:");
@@ -68,31 +83,32 @@ class Program {
 
       start = DateTime.Now;
 
-      psum = 0d;
+      // Array to store the output of the chunks.
+      double[] parallelsum = new double[numthreads];
+
       step = 1d/(double)numsteps;
 
-      Parallel.For<double>(0, numthreads, options, () => 0d, (j, loop, psub) => {
+      // Actual parallel loop.
+      Parallel.For(0, numthreads, options, j => {
          // Do decomposition
          // Note: to make variables explicitly local we need to declare them inside.
-         long sta = j * (numsteps/numthreads);
-         long sto = ((j + 1) * (numsteps/numthreads)) - 1;
+         long lower = j * (numsteps/numthreads);
+         long upper = ((j + 1) * (numsteps/numthreads)) - 1;
          if (j == numthreads - 1) {
-            sto = numsteps -1;
+            upper = numsteps -1;
          }
          double x = 0d;
+         double subtotal = 0;
 
-         for (long i = sta; i <= sto; i++){
+         for (long i = lower; i <= upper; i++){
             x = ((double)i + 0.5d) * step;
-            psub += 4d/(1d + (x * x));
+            subtotal += 4d/(1d + (x * x));
          }
-         DebugMessage(j + ": " + sta + " => " + sto + " == " + psub);
+         DebugMessage(j + ": " + lower + " => " + upper + " == " + subtotal);
+         parallelsum[j] = subtotal;
+      });
 
-         return psub;
-      }, // Do reduction horribly.
-         (y) => {lock (z) {psum += y;}}
-      );
-
-      mypi = psum * step;
+      mypi = parallelsum.Sum() * step;
 
       stop = DateTime.Now;
       difference = stop.Subtract(start);
@@ -101,7 +117,5 @@ class Program {
 
       Console.WriteLine("Obtained value of PI: " + mypi);
       Console.WriteLine("Time taken: " + time + " seconds");
-
    }
-
 }
