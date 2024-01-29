f5cd78fb50f8be8179502ed46ed0f73059ec4516
Owain Kenway <o.kenway@ucl.ac.uk>
2024-01-29 14:09:28 +0000

    C++ OpenACC implementation

---
+++ cpp_openacc_pi_dir/calculate_pi.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <limits>
+
+#include "timer.hpp"
+
+using namespace std;
+
+int main( int argc, char **argv ) {
+   long i, num_steps = 1000000000;
+
+   cout.precision(numeric_limits<double>::digits10+2);
+   
+   if (argc > 1) {
+      num_steps = atol(argv[1]);
+   }
+
+   double step, x, sum, pi;
+   Timer timer;
+   
+   cout << "Calculating PI using:" << endl <<
+           "  " << num_steps << " slices" << endl <<
+           "  1 process" << endl;
+   
+   timer.start();
+   
+   sum = 0.0;
+   step = 1.0 / num_steps;
+
+#pragma acc kernels
+   for (i=0;i<num_steps;i++) {
+      x    = (i + 0.5) * step;
+      sum += 4.0 / (1.0 + x*x);
+   }
+
+   pi = sum * step;
+
+   timer.stop();
+
+   cout << "Obtained value for PI: " << pi << endl <<
+           "Time taken: " << timer.duration() << " seconds" << endl;
+
+   return 0;
+}
+
