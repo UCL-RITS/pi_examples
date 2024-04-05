09583ca09c8e65de4fa3122e1340bea0140113da
Owain Kenway <o.kenway@ucl.ac.uk>
2024-04-05 15:24:35 +0100

    Less(?) horrific casting.

--- zig_pi_dir/src/main.zig
+++ zig_pi_dir/src/main.zig
@@ -27,22 +27,20 @@ pub fn main() !void {
 	const start = try std.time.Instant.now();
 
 	var s: f64 = 0.0;
-	var n_slices_f: f64 = @floatFromInt(n_slices); // Really?
-	var step: f64 = 1.0 / n_slices_f;
+	var step: f64 = 1.0 / @as(f64, @floatFromInt(n_slices));
 	var x: f64 = 0.0;
 
 	for (1..n_slices) |i| {
-		var i_f: f64 = @floatFromInt(i); // REALLY??!!
-		x = (i_f - 0.5) * step;
-		s = s + 4.0 / (1.0 + x*x);
+		x = (@as(f64, @floatFromInt(i)) - 0.5) * step;
+		s = s + 4.0 / (1.0 + x * x);
 	}
 
 	const p: f64 = s * step;
 	const finish = try std.time.Instant.now();
-	const elapsed: f64 = @floatFromInt(finish.since(start));
+	const elapsed: f64 = @as(f64, @floatFromInt(finish.since(start)))/1000000000.0;
 
 	try stdout.print("Estimated value of π: {d}\n", .{p});	
-	try stdout.print("Elapsed time {d} seconds.\n", .{elapsed/1000000000.0});	
+	try stdout.print("Elapsed time {d} seconds.\n", .{elapsed});	
 
 	try bw.flush(); // don't forget to flush!
 }
